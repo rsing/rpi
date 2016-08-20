23f7120c14110b28044ca1b455b18e625fdb2a6f
pi <pi@raspberrypi>
2016-08-20 22:13:25 +0000

    Changed pin numbers

--- blink1.py
+++ blink1.py
@@ -7,7 +7,7 @@ gpio.setmode(gpio.BCM)
 
 pins = {
     3: {'name': 'GPIO 02', 'state': gpio.LOW},
-    5: {'name': 'GPIO 03', 'state': gpio.LOW}
+    2: {'name': 'GPIO 03', 'state': gpio.LOW}
     }
 
 for pin in pins:
@@ -15,9 +15,9 @@ for pin in pins:
     gpio.setup(pin, gpio.LOW)
 
 for i in range(5):
-    time.sleep(2)
+    time.sleep(0.3)
     for pin in pins:
         gpio.output(pin, gpio.HIGH)
-    time.sleep(2)
+    time.sleep(0.3)
     for pin in pins:
         gpio.output(pin, gpio.LOW)
