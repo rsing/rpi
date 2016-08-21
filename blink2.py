33d73d267e4e3ca25606e4cf37afeccffc1d2ca2
Rahul Singh <randforest@outlook.com>
2016-08-21 00:28:49 -0500

    Removed new.txt

---
+++ blink2.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as gpio
+import time
+import random
+from flask import Flask, render_template, request
+
+app=Flask(__name__)
+gpio.setmode(gpio.BCM)
+
+pins = {
+    3: {'name': 'GPIO 02', 'state': gpio.LOW},
+    2: {'name': 'GPIO 03', 'state': gpio.LOW}
+    }
+
+for pin in pins:
+    gpio.setup(pin, gpio.OUT)
+    gpio.setup(pin, gpio.LOW)
+
+for i in range(50):
+    for pin in pins:
+        time.sleep(random.random())
+        gpio.output(pin, gpio.HIGH)
+    for pin in pins:
+        time.sleep(random.random())
+        gpio.output(pin, gpio.LOW)
