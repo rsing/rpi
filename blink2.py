4626b5ee1cb6bc7c11b28d911114fa85e34ce34e
rahuls <randforest@outlook.com>
2016-08-21 11:54:32 -0500

    Add flask

--- blink2.py
+++ blink2.py
@@ -17,11 +17,30 @@ for pin in pins:
     gpio.setup(pin, gpio.OUT)
     gpio.setup(pin, gpio.LOW)
 
+# Set up flask to serve html
+@app.route("/<my_pin>/<my_action>")
+def pin_action(my_pin, my_action):
+    if my_action.lower() == "off":
+        gpio.output(my_pin, gpio.LOW)
+    elif my_action.lower() == "on":
+        gpio.output(my_pin, gpio.LOW)
+
+@app.route("/gocrazy")
+def crazy_action1():
+    blink_uncontrollably(1.0)
+
+@app.route("/goverycrazy")
+def crazy_action2():
+    blink_uncontrollably(3.0)
+
 # Blink uncontrollably
-for i in range(50):
+def blink_uncontrollably(delay_factor):
+    for i in range(50):
+        for pin in pins:
+            time.sleep(random.random()/delay_factor)
+            gpio.output(pin, gpio.HIGH)
+        for pin in pins:
+            time.sleep(random.random()/delay_factor)
+            gpio.output(pin, gpio.LOW)
     for pin in pins:
-        time.sleep(random.random())
-        gpio.output(pin, gpio.HIGH)
-    for pin in pins:
-        time.sleep(random.random())
         gpio.output(pin, gpio.LOW)
