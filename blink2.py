136ce23cc240cb6445d12f1f810f2482ccc00833
RPI <randforest@outlook.com>
2016-08-27 17:39:06 -0500

    new blink

--- blink2.py
+++ blink2.py
@@ -3,20 +3,19 @@ import time
 import random
 from flask import Flask, render_template, request
 
+app=Flask(__name__)
 gpio.setmode(gpio.BCM)
 
-# Store the pins that we will work with
 pins = {
-    3: {'name': 'GPIO 02', 'state': gpio.LOW},
-    2: {'name': 'GPIO 03', 'state': gpio.LOW}
-    }
+	4: {'name': 'GPIO 02', 'state': gpio.LOW},
+	14: {'name': 'GPIO 03', 'state': gpio.LOW}
+	}
 
-# Setup the pins
-for pin in pins:
-    gpio.setup(pin, gpio.OUT)
-    gpio.setup(pin, gpio.LOW)
-
-app=Flask(__name__)
+def setup_pi():
+	print "Setting up LEDs.."
+	for pin in pins:
+		gpio.setup(pin, gpio.OUT)
+		gpio.setup(pin, gpio.LOW)
 
 # Set up flask to serve html
 @app.route("/<my_pin>/<my_action>")
@@ -32,23 +31,27 @@ def crazy_action1():
 
 @app.route("/goverycrazy")
 def crazy_action2():
-    blink_uncontrollably(3.0)
+    blink_uncontrollably(10.0)
 
 @app.route("/off")
 def turn_off():
-    for pin in pins:
-        gpio.output(pin, gpio.LOW)
-
+	gpio.cleanup()
+	
 # Blink uncontrollably
 def blink_uncontrollably(delay_factor):
-    for i in range(50):
-        for pin in pins:
-            time.sleep(random.random()/delay_factor)
-            gpio.output(pin, gpio.HIGH)
-        for pin in pins:
-            time.sleep(random.random()/delay_factor)
-            gpio.output(pin, gpio.LOW)
-    #gpio.cleanup()
+	try:
+		while True:
+			for pin in pins:
+				time.sleep(random.random()/delay_factor)
+				gpio.output(pin, gpio.HIGH)
+			for pin in pins:
+				time.sleep(random.random()/delay_factor)
+				gpio.output(pin, gpio.LOW)
+	except KeyboardInterrupt:
+		print "Interrupt!"
+	finally:
+		gpio.cleanup()			
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+	setup_pi()	
+	app.run(debug=True,host='0.0.0.0')
